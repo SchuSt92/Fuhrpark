ba7dc5ae6c946f918c5c82aef0314c63a2d090c2
SchuSt92 <steffen.schulz1992@icloud.com>
2022-02-08 00:25:16 +0100

    Neue Init

---
+++ Fuhrpark/src/main/java/Fuhrpark/Fuhrpark/App.java
@@ -0,0 +1,13 @@
+package Fuhrpark.Fuhrpark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
