8c99575b4caaaf20322fd03ba3346f0a4e61d782
SchuSt92 <steffen.schulz1992@icloud.com>
2022-02-08 11:50:44 +0100

    Commit Mainframe erstellt

--- Fuhrpark/src/main/java/FuhrparkApp/App.java
+++ Fuhrpark/src/main/java/FuhrparkApp/App.java
@@ -1,12 +1,13 @@
 package FuhrparkApp;
 
+import Gui.MainFrame;
 
 public class App 
 {
     public static void main( String[] args )
     {
        
-
+    	MainFrame mainFrame = new MainFrame();
     	
     }
 }
